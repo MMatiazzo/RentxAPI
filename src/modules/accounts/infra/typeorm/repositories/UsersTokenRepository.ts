f5307d9ae800ffb29b963db65144d8d54677631f
MMatiazzo <mathmatiazzo@gmail.com>
2023-01-25 16:02:55 -0300

    feat: refresh token on authentication

--- src/modules/accounts/infra/typeorm/repositories/UsersTokenRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokenRepository.ts
@@ -22,6 +22,19 @@ class UsersTokensRepository implements IUsersTokensRepository {
         return userTokens;
     }
 
+    async findByUserId(user_id: string): Promise<UserTokens[]> {
+        const userToken = this.repository.find({user_id});
+        return userToken; 
+    }
+
+    async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+        const userToken = this.repository.findOne({user_id, refresh_token});
+        return userToken; 
+    }
+
+    async deleteById(id: string): Promise<void> {
+        await this.repository.delete(id);
+    }
 }
 
 export { UsersTokensRepository }
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,39 +2,46 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokenRepository";
+import auth from "@config/auth";
 
 interface IPayload {
     sub: string;
-} 
+}
 
-export async function ensureAuthenticated (
-    request: Request, 
-    response: Response, 
+export async function ensureAuthenticated(
+    request: Request,
+    response: Response,
     next: NextFunction
-){
+) {
 
     const authHeader = request.headers.authorization;
 
-    if(!authHeader) {
+    const usersTokensRepository = new UsersTokensRepository();
+
+    if (!authHeader) {
         throw new AppError("Token missing", 401);
     }
 
     const [, token] = authHeader.split(" ");
 
-    try{
-       const { sub: user_id } = verify(token, "225639b3444e5150227e1f6b5d15f2a2") as IPayload;
-       
-       const usersRepository = new UsersRepository();
-       const user = usersRepository.findById(user_id);
 
-       if(!user) {
-        throw new AppError("User does not exists", 401);
-       }
-       request.user = {
-        id: user_id
-       };
-       next();
-    }catch {
+    try {
+        const { sub: user_id } = verify(
+            token,
+            auth.secret_refresh_token
+        ) as IPayload;
+
+        const user = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
+
+        if (!user) {
+            throw new AppError("User does not exists", 401);
+        }
+        request.user = {
+            id: user_id
+        };
+        next();
+    } catch {
         throw new AppError("Invalid token", 401);
     }
 }
