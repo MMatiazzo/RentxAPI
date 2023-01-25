d46063bae6e93eb56ed36c8bf3e61bbb97c57a7a
MMatiazzo <mathmatiazzo@gmail.com>
2023-01-25 14:19:07 -0300

    feat: refresh token on user session

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "225639b3444e5150227e1f6b5d15f2a2",
+    expires_in_token: "15m",
+    secret_refresh_token: "495e74e34c31a4e651d1b7b859f6badb",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30
+}
